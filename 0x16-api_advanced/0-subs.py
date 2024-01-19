9ad0293209908095f8894cfdb27c66249343fcb9
Abdou_juinor <aboudiwac9@gmail.com>
2024-01-19 11:11:08 +0000

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""
-Contains the number_of_subscribers function
-"""
 
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
-        return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+	"""Return the total number of subscribers on a given subreddit."""
+	url = "htpps://www.reddit.com/r/{}/about.json".format(subreddit)
+	headers = {
+		"User-Agent": "Linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+	}
+	response = request.get(url, headers=headers, allow_redirects=False)
+	if response.status_code == 404:
+		return 0
+	results =response.json().get("data")
+	return results.get("subscribers")
