4a23cf43b8b90cb17e1479096586312b28348652
EL-HOUSS-BRAHIM <bross.or.of@gmail.com>
2024-08-31 15:36:35 +0100

    100-count.py

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" last_task """
+import requests
+
+
+def count_words(subreddit, word_list):
+    """
+    parses the title of all hot articles, and prints a sorted count
+    of given keywords (case-insensitive, delimited by spaces.
+    """
+    pass
