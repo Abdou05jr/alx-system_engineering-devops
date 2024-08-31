588f974d8caaf4a2f2ffe55f43c9cd75fb8a4a81
EL-HOUSS-BRAHIM <bross.or.of@gmail.com>
2024-08-31 15:34:05 +0100

    1-top_ten.py

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" task 3 """
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    """
+    returns a list containing the titles of
+    all hot articles for a given subreddit.
+    """
+    pass
